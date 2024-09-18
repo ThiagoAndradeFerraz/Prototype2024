02fdbf4d0215ef41ec882acb48a197f2d5e9693c
Thiago Ferraz <thiagoaferrazcontato@gmail.com>
2024-09-17 22:52:45 -0300

    roda do invetnario funcional

--- UI/Inventory/scripts/inventory_ui.gd
+++ UI/Inventory/scripts/inventory_ui.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
+var center_item_index : int = 0
+
+var slot_left : Sprite2D
+var slot_center : Sprite2D
+var slot_right : Sprite2D
+
 func _ready():
-	
+	slot_left = $"inventoy_control/control_img_left/Sprite2D"
+	slot_center = $"inventoy_control/control_img_center/Sprite2D"
+	slot_right = $"inventoy_control/control_img_right/Sprite2D"
+
 	visible = false
 
 func _unhandled_input(event):
@@ -10,53 +19,101 @@ func _unhandled_input(event):
 
 		if(event.is_action_pressed("open_inventory") and not get_tree().paused):
 			
-			show_inventory()
+			if(visible):
+				close_inventory()
+			else:
+				show_inventory()
 
 		elif(event.is_action_pressed("pause")):
 			
-			visible = false
+			close_inventory()
 
 		elif(event.is_action_pressed("select")):
-			
-			if(visible):
+
+			if(visible): # command valid only if inventory is opened
 				select_item()			
 
+		elif(event.is_action_pressed("move_right")):
+
+			if(visible): # command valid only if inventory is opened
+				if(InventoryManager.inventory.size() > 0):
+					center_item_index = center_item_index + 1
+
+					load_items()
+		
+		elif(event.is_action_pressed("move_left")):
+
+			if(visible): # command valid only if inventory is opened
+				if(InventoryManager.inventory.size() > 0):
+					center_item_index = center_item_index - 1
+
+					load_items()
 
 func select_item():
-
-	'''
-	TODO: Implementar os seguintes cenários:
-					
-		* Seleção de item sem comparação com algum esperado
-
-		* Seleção de item com comparação de algum item esperado
-			* Desconenctar o objeto a ser comparado do sinal
-	'''
 
 	if(InventoryManager.expected_item != null):
 		
 		print(InventoryManager.expected_item.name)
+		
+		# TODO: compare item. Maybe pass the selected item as argument?
 		EventManager.notify_interacted_using_inv_item()
-		show_inventory()
+		
+		close_inventory()
 
 	else:
-
 		print("PLAYER: consumiu item")
+		close_inventory()
 
 	pass
-
 
 func show_inventory():
 
 	# TODO: precisa impedir que o jogador ande enquanto estiver aberto
+	load_items()
+	visible = true
 
-	'''
-	TODO:
-		* carregar dados do inventário
-		* impedir que o jogador ande enquanto estiver com o inventario aberto
-	'''
+func close_inventory():
 
-	visible = not visible
+	visible = false
+	InventoryManager.set_expected_item(null) # reseting the expected item
 
-	if(not visible):
-		InventoryManager.set_expected_item(null) # reseting the expected item when closing inventory
+func load_items():
+
+	if(InventoryManager.inventory.size() == 0):
+		return
+
+	if(center_item_index > InventoryManager.inventory.size() - 1):
+		center_item_index = 0
+	
+	elif (center_item_index < 0):
+		center_item_index = InventoryManager.inventory.size() - 1
+
+	# fill the center
+
+	slot_center.texture = InventoryManager.inventory[center_item_index].inventory_icon
+	slot_center.scale.x = 0.257
+	slot_center.scale.y = 0.257
+
+	# if has right, fill right
+
+	if(InventoryManager.inventory.size() > 1):
+
+		if(center_item_index == InventoryManager.inventory.size() - 1):
+			slot_right.texture = InventoryManager.inventory[0].inventory_icon
+		else:
+			slot_right.texture = InventoryManager.inventory[center_item_index + 1].inventory_icon
+			
+		slot_right.scale.x = 0.155
+		slot_right.scale.y = 0.155
+
+	# if has 3 or more, fill left too
+
+	if(InventoryManager.inventory.size() >= 3):
+
+		if(center_item_index == 0):
+			slot_left.texture = InventoryManager.inventory[InventoryManager.inventory.size() - 1].inventory_icon
+		else:
+			slot_left.texture = InventoryManager.inventory[center_item_index - 1].inventory_icon
+			
+		slot_left.scale.x = 0.155
+		slot_left.scale.y = 0.155
